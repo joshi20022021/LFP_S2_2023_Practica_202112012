79ce7e48bd79f75f6610d41e4c195b14aae3a825
Edgar Josías Cán Ajquejay <113130705+joshi20022021@users.noreply.github.com>
2023-08-22 23:57:07 -0600

    Add files via upload

---
+++ LFP_S2_2023_Practica_202112012/MANUALFINAL/MANUALFINAL.md
@@ -0,0 +1,128 @@
+﻿
+
+
+
+
+
+
+
+
+
+
+**MANUAL** 
+
+**TECNICO**
+
+
+![](Aspose.Words.adde0b31-c594-467d-af44-a33747c21056.001.png)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+PROGRAMAS UTILIZADOS
+
+VISUAL STUDIO CODE
+
+![](Aspose.Words.adde0b31-c594-467d-af44-a33747c21056.002.png)![File:Visual Studio Code 1.35 icon.svg - Wikimedia Commons](Aspose.Words.adde0b31-c594-467d-af44-a33747c21056.003.png)
+
+WINDOWS 10
+![](Aspose.Words.adde0b31-c594-467d-af44-a33747c21056.004.png)![File:Windows logo - 2012.svg - Wikimedia Commons](Aspose.Words.adde0b31-c594-467d-af44-a33747c21056.005.png)
+
+
+
+
+
+PYTHON
+
+![](Aspose.Words.adde0b31-c594-467d-af44-a33747c21056.006.png)![Archivo:Python-logo-notext.svg - Wikipedia, la enciclopedia libre](Aspose.Words.adde0b31-c594-467d-af44-a33747c21056.007.png)
+
+
+
+
+
+
+
+Estructuras de lectura
+
+Prueba.inv
+
+![](Aspose.Words.adde0b31-c594-467d-af44-a33747c21056.008.png)
+
+prueba.mov
+
+![](Aspose.Words.adde0b31-c594-467d-af44-a33747c21056.009.png)
+
+**Función cargar\_Inventario\_Inicial**
+
+Imprime un encabezado y un mensaje para indicar que se está cargando el inventario inicial.
+
+Pide al usuario que ingrese la ruta del archivo .inv que contiene los detalles de los productos.
+
+![ref1]
+
+Abre el archivo en modo de lectura y comienza a leer las líneas del archivo. Para cada línea en el archivo: Divide la línea en dos partes utilizando el primer espacio como separador. La primera parte es la instrucción (por ejemplo, "crear\_producto") y la segunda parte es la cadena de parámetros. Si la instrucción es "crear\_producto", intenta dividir los parámetros en nombre, cantidad, precio unitario y ubicación
+
+![ref2]
+
+Si los valores no se pueden convertir correctamente a números, se maneja la excepción y se ignora la entrada. Comprueba si el nombre del producto ya existe en el inventario. Si existe y la ubicación coincide, agrega la cantidad al producto existente.
+
+![ref3]Si existe pero la ubicación es diferente, agrega el sufijo "(i)" al nombre (donde i es un número incremental) para crear un nuevo nombre y agrega el producto con la nueva clave. Si no existe, agrega el producto al inventario con la clave del nombre y los detalles correspondientes.
+
+
+
+![](Aspose.Words.adde0b31-c594-467d-af44-a33747c21056.013.png)Imprime mensajes de confirmación y finaliza la función después de procesar todas las líneas del archivo. Si el archivo no se encuentra, maneja la excepción y muestra un mensaje indicando que el archivo no fue encontrado.
+
+**Funcion cargar\_instrucciones\_movimientos**
+
+![ref4]Imprime un encabezado y un mensaje para indicar que se está cargando las instrucciones de movimientos. Pide al usuario que ingrese la ruta del archivo .mov que contiene las instrucciones de movimientos. Abre el archivo en modo de lectura y comienza a leer las líneas del archivo.
+
+Elimina espacios en blanco al principio y al final de la línea. Si la línea está vacía, la ignora y pasa a la siguiente. Divide la línea en dos partes utilizando el primer espacio como separador. La primera parte es la instrucción (por ejemplo, "agregar\_stock" o "vender\_producto"), y la segunda parte son los detalles que contienen el nombre del producto, la cantidad y la ubicación. Divide los detalles en tres partes utilizando el punto y coma como separador. Si no hay exactamente tres partes, muestra un mensaje de error y continúa con la siguiente línea. Intenta convertir la cantidad de productos a un número entero. Si esto falla, muestra un mensaje de error y continúa con la siguiente línea
+
+![ref5]
+
+Verifica si el nombre del producto existe en el inventario. Si no existe, muestra un mensaje de error y continúa con la siguiente línea. Si existe pero la ubicación no coincide, muestra un mensaje de error y continúa con la siguiente línea. Si la instrucción es "agregar\_stock", agrega la cantidad especificada al producto en el inventario. Si la instrucción es "vender\_producto", verifica si hay suficiente cantidad de producto en el inventario para vender. Si hay suficiente, resta la cantidad especificada del producto en el inventario. Si la instrucción no se reconoce, muestra un mensaje de error. Si el archivo no se encuentra, maneja la excepción y muestra un mensaje indicando que el archivo no fue encontrado.
+
+![](Aspose.Words.adde0b31-c594-467d-af44-a33747c21056.016.png)
+
+
+
+**Funcion crear\_informe\_inventario**
+
+Solicita al usuario que ingrese el nombre del archivo para guardar el informe. Este nombre será usado como parte del nombre del archivo de salida. 
+
+Crea el nombre completo del archivo de salida agregando la extensión .txt al nombre ingresado por el usuario y escribe dentro de el. Posteriormente genera una tabla en el archivo .txt creado. posteriormente recorre cada producto en el inventario buscando en los diccionario el nombre del producto y sus detalles. Calcula el valor total del producto multiplicando la cantidad por el precio unitario. Suma el valor total del producto al valor total acumulado. Escribe una línea en el archivo que contiene los detalles del producto en columnas de la tabla. Cierra el archivo. Si ocurre un error al crear el archivo (por ejemplo, si no se tiene permisos de escritura en la ubicación especificada), muestra un mensaje de error.
+
+![](Aspose.Words.adde0b31-c594-467d-af44-a33747c21056.017.png)
+
+
+
+
+
+
+
+**Funcion main**
+
+implementa un menú interactivo y cíclico que permite al usuario gestionar el sistema de inventario. El programa proporciona opciones para cargar productos en el inventario, realizar movimientos de productos (agregar o vender), crear informes detallados del inventario y salir del programa al ingresar a cada una ejecutara la función a la cual está integrada.
+
+![](Aspose.Words.adde0b31-c594-467d-af44-a33747c21056.018.png)
+
+
+[ref1]: Aspose.Words.adde0b31-c594-467d-af44-a33747c21056.010.png
+[ref2]: Aspose.Words.adde0b31-c594-467d-af44-a33747c21056.011.png
+[ref3]: Aspose.Words.adde0b31-c594-467d-af44-a33747c21056.012.png
+[ref4]: Aspose.Words.adde0b31-c594-467d-af44-a33747c21056.014.png
+[ref5]: Aspose.Words.adde0b31-c594-467d-af44-a33747c21056.015.png
--- LFP_S2_2023_Practica_202112012/main.py
+++ LFP_S2_2023_Practica_202112012/main.py
@@ -11,19 +11,39 @@ def cargar_inventario_inicial():
             for linea in lineas:
                 instruccion, parametros = linea.strip().split(' ', 1)
                 if instruccion == 'crear_producto':
-                    nombre, cantidad_str, precio_unitario_str, ubicacion = parametros.split(';')
-                    cantidad = float(cantidad_str)
-                    precio_unitario = float(precio_unitario_str)
+                    try:
+                        nombre, cantidad_str, precio_unitario_str, ubicacion = parametros.split(';')
+                        cantidad = float(cantidad_str)
+                        precio_unitario = float(precio_unitario_str)
+                    except ValueError:
+                        print(f"Error en el formato de línea: {linea}. Ignorando entrada.")
+                        continue
                     
-                    # Comprobar si el producto ya existe en el inventario
                     if nombre in inventario:
-                        print(f"Producto '{nombre}' ya existe en el inventario. Ignorando entrada.")
+                        if ubicacion == inventario[nombre]['ubicacion']:
+                            inventario[nombre]['cantidad'] += cantidad
+                            print(f"Se agregaron {cantidad:.2f} unidades de '{nombre}' a '{ubicacion}'.")
+                        else:
+                            
+                            i = 2
+                            new_nombre = nombre
+                            while new_nombre in inventario:
+                                new_nombre = f"{nombre} ({i})"
+                                i += 1
+                            inventario[new_nombre] = {
+                                'cantidad': cantidad,
+                                'precio_unitario': precio_unitario,
+                                'ubicacion': ubicacion
+                            }
+                            print(f"Producto '{new_nombre}' agregado al inventario en la ubicación '{ubicacion}'.")
                     else:
                         inventario[nombre] = {
                             'cantidad': cantidad,
                             'precio_unitario': precio_unitario,
                             'ubicacion': ubicacion
                         }
+                        print(f"Producto '{nombre}' agregado al inventario en la ubicación '{ubicacion}'.")
+                    
             print("------------------------------------------------------------------")
             print("Inventario inicial cargado exitosamente.")
             return inventario
@@ -43,45 +63,39 @@ def cargar_instrucciones_movimientos(inventario):
             
             for linea in lineas:
                 linea = linea.strip()
-                if not linea:  # Ignorar líneas vacías
+                if not linea:  
                     continue
                 
-                instruccion, detalles = linea.split(' ', 1)
-                
-                # Verificar si hay suficientes componentes en los detalles
+                instruccion, detalles = linea.split(' ', 1) 
                 detalles_partes = detalles.split(';')
-                if len(detalles_partes) < 3:
+                if len(detalles_partes) != 3:
                     print(f"Error en el formato de línea: {linea}. Ignorando entrada.")
                     continue
                 
+                nombre, cantidad_str, ubicacion = detalles_partes
                 try:
-                    nombre, cantidad_str, ubicacion = detalles_partes
-                    cantidad = float(cantidad_str)
+                    cantidad = int(cantidad_str)
                 except ValueError:
                     print(f"Error en el formato de detalles en línea: {linea}. Ignorando entrada.")
                     continue
                 
+                if nombre not in inventario:
+                    print(f"No se pudo ejecutar la instrucción para {nombre}. Producto no existe en el inventario.")
+                    continue
+                
+                if ubicacion != inventario[nombre]['ubicacion']:
+                    print(f"No se pudo ejecutar la instrucción para {nombre}. Ubicación incorrecta.")
+                    continue
+                
                 if instruccion == 'agregar_stock':
-                    if nombre in inventario:
-                        if ubicacion == inventario[nombre]['ubicacion']:
-                            inventario[nombre]['cantidad'] += cantidad
-                            print(f"Se agregaron {cantidad:.2f} unidades de {nombre} a {ubicacion}.")
-                        else:
-                            print(f"No se pudo agregar stock de {nombre} a {ubicacion}. Ubicación incorrecta.")
-                    else:
-                        print(f"No se pudo agregar stock de {nombre}. Producto no existe en el inventario.")
+                    inventario[nombre]['cantidad'] += cantidad
+                    print(f"Se agregaron {cantidad:.2f} unidades de {nombre} a {ubicacion}.")
                 elif instruccion == 'vender_producto':
-                    if nombre in inventario:
-                        if ubicacion == inventario[nombre]['ubicacion']:
-                            if cantidad <= inventario[nombre]['cantidad']:
-                                inventario[nombre]['cantidad'] -= cantidad
-                                print(f"Se vendieron {cantidad:.2f} unidades de {nombre} de {ubicacion}.")
-                            else:
-                                print(f"No hay suficiente stock de {nombre} en {ubicacion}.")
-                        else:
-                            print(f"No se pudo vender {nombre} en {ubicacion}. Ubicación incorrecta.")
+                    if cantidad <= inventario[nombre]['cantidad']:
+                        inventario[nombre]['cantidad'] -= cantidad
+                        print(f"Se vendieron {cantidad:.2f} unidades de {nombre} de {ubicacion}.")
                     else:
-                        print(f"No se pudo vender {nombre}. Producto no existe en el inventario.")
+                        print(f"No hay suficiente stock de {nombre} en {ubicacion}.")
                 else:
                     print(f"Instrucción no reconocida: {instruccion}")
     except FileNotFoundError:
@@ -99,21 +113,20 @@ def crear_informe_inventario(inventario):
             archivo.write("╔═════════════════════════════════════════════════════════════════════════════════╗\n")
             archivo.write("║ Producto   ║   Cantidad   ║   Precio Unitario   ║   Valor Total   ║ Ubicación   ║\n")
             archivo.write("╠═════════════════════════════════════════════════════════════════════════════════╣\n")
-
+            total_valor = 0
             for producto, detalles in inventario.items():
                 cantidad = detalles["cantidad"]
                 precio_unitario = detalles["precio_unitario"]
                 ubicacion = detalles["ubicacion"]
                 valor_total = cantidad * precio_unitario
-                
+                total_valor += valor_total
                 archivo.write(f"║ {producto:<10} ║ {cantidad:>12} ║ {precio_unitario:>20.2f} ║ {valor_total:>15.2f} ║ {ubicacion:<10} ║\n")
             archivo.write("╚═════════════════════════════════════════════════════════════════════════════════╝\n")
-            
-            print(f"Informe de inventario creado en el archivo '{archivo_salida}'.")
-            
+            archivo.write(f"Total Valor: {total_valor:>67.2f}\n")      
+        print(f"Informe de inventario creado en el archivo '{archivo_salida}'.")
+    
     except IOError:
         print("Error al crear el informe de inventario.")
-
 
 def main():
     inventario = {}
